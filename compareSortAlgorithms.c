c52d9659b0cb554234606627363aa532724b807a
daniel <da037920@ucf.edu>
2024-03-24 16:16:50 -0400

    updated sorting functions

--- compareSortAlgorithms.c
+++ compareSortAlgorithms.c
@@ -36,13 +36,94 @@ void heapSort(int arr[], int n)
 // extraMemoryAllocated counts bytes of extra memory allocated
 void mergeSort(int pData[], int l, int r)
 {
-	
+	if(l < r){
+
+		//sorting array
+		//find the middle of pData array and assign the value to variable m
+		int m = (l + r)/2;
+
+		//recursive call
+		mergeSort(pData, l, m);
+		mergeSort(pData, m+1, r);
+
+		//merge array
+		// variables i, j, and k are counters
+		int i, j, k;
+		//Number of elements in two subarrays after pData is split at m
+		int n1 = m-l+1;
+		int n2 = r-m;
+
+		// create temp arrays
+		int *L = (int*)Alloc(sizeof(int)* n1);
+		int *R = (int*)Alloc(sizeof(int) * n2);
+
+		//copy data to temp arrays L and R
+		for ( i = 0; i < n1; i++)
+			L[i] = pData[l+i];
+		for ( j = 0; j < n2; i++)
+			R[j] = pData[m + 1 + j];
+
+		//merge the temp arrays back
+		i = 0;
+		j = 0;
+		k = 0;
+		while(i < n1 && j < n2){
+			if(L[i]<= R[j]){
+				pData[k]= L[i];
+				i++;
+			}
+			else{
+				pData[k] = R[j];
+				j++;
+			}
+			k++;
+		}
+		// copy remaining elements of L if any
+		while(i < n1){
+			pData[k] = L[i];
+			i++;
+			k++;
+		}
+
+		// copy remaining elements ofR if any
+		while(j < n2){
+			pData[k] = R[j];
+			j++;
+			k++;
+		}
+
+		//free subarrays
+		DeAlloc(L);
+		DeAlloc(R);
+
+		
+	}
+
 }
 
 // implement insertion sort
 // extraMemoryAllocated counts bytes of memory allocated
 void insertionSort(int* pData, int n)
 {
+	//variables i and j are counters and item is the element that will be the key
+	int i, item, j;
+	for ( i = 1; i < n; i++)
+	{
+		item = pData[i];
+		for ( j = i-1; j>=0; j--)
+		{
+			//move element to one position ahead if its greater than the key
+			if(pData[j]>item){
+				pData[j+1] = pData[j];
+			}
+			else
+				break;
+		}
+
+		pData[j + 1] =item;
+		
+	}
+	
 	
 }
 
@@ -50,6 +131,25 @@ void insertionSort(int* pData, int n)
 // extraMemoryAllocated counts bytes of extra memory allocated
 void bubbleSort(int* pData, int n)
 {
+	//declare variables i and j for counters, 
+	//and temp to temporary hold the value that is being switched.
+	int i, j, temp;
+	for (i = 0; i < n-1; i++)
+	{
+		//loop through unsorted array setting the control statement to last sorted element
+		for ( j = 0; j < n-i-1; j++)
+		{
+			//swap the current element with the next if is greater than the next element
+			if(pData[j]> pData[j+1]){
+				temp=pData[j];
+				pData[j] = pData[j+1];
+				pData[j+1] = temp;
+
+			}
+		}
+		
+	}
+	
 	
 }
 
@@ -57,6 +157,29 @@ void bubbleSort(int* pData, int n)
 // extraMemoryAllocated counts bytes of extra memory allocated
 void selectionSort(int* pData, int n)
 {
+	//declare variables i and j for counters, min_idx to store the minimum idex, 
+	//and temp to temporary hold the value that is being switched.
+	int i, j, min_idx, temp;
+
+	//move the boundary of the array
+	for ( i = 0; i < n-1; i++)
+	{
+		//find the minimum element
+		min_idx = i;
+		for ( j = i+1; j < n; j++)
+		
+		
+			if(pData[j]< pData[min_idx])
+				min_idx = j;
+			
+			//swap the minimum element with the first element
+			temp = pData[i];
+			pData[i] = pData[min_idx];
+			pData[min_idx]= temp;
+			
+		
+	}
+	
 	
 }
 
@@ -72,6 +195,11 @@ int parseData(char *inputFileName, int **ppData)
 		fscanf(inFile,"%d\n",&dataSz);
 		*ppData = (int *)Alloc(sizeof(int) * dataSz);
 		// Implement parse data block
+		for (int i = 0; i < dataSz; i++)
+		{
+			fscanf(inFile, "%d", *ppData+i);
+		}
+		
 	}
 	
 	return dataSz;
@@ -82,6 +210,7 @@ void printArray(int pData[], int dataSz)
 {
 	int i, sz = dataSz - 100;
 	printf("\tData:\n\t");
+
 	for (i=0;i<100;++i)
 	{
 		printf("%d ",pData[i]);
@@ -164,7 +293,7 @@ int main(void)
 		memcpy(pDataCopy, pDataSrc, dataSz*sizeof(int));
 		extraMemoryAllocated = 0;
 		start = clock();
-		heapSort(pDataCopy, 0, dataSz - 1);
+		//heapSort(pDataCopy, 0, dataSz - 1);
 		end = clock();
 		cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
 		printf("\truntime\t\t\t: %.1lf\n",cpu_time_used);
